4e61ee0da917432f2bd1ab2afa6d92ae3253e4e8
Justin Davis <justn.davis@gmail.com>
2019-10-25 21:50:53 -0400

    Correcting the plot

--- mpl_squares.py
+++ mpl_squares.py
@@ -1,9 +1,10 @@
 import matplotlib.pyplot as plt
 
-squares = [x**2 for x in range(1,6)]
+input_values = [x for x in range(1,6)]
+squares = [x**2 for x in input_values]
 
 fig, ax = plt.subplots()
-ax.plot(squares, linewidth=3)
+ax.plot(input_values, squares, linewidth=3)
 
 #Set chart title and label axes.
 ax.set_title("Square Numbers", fontsize=24)
