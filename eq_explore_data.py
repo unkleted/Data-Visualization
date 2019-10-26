b0a946064deff2f49de97d5ec37fd385d4268dac
Justin Davis <justn.davis@gmail.com>
2019-10-26 16:18:03 -0400

    making a list of all earthquakes

--- eq_explore_data.py
+++ eq_explore_data.py
@@ -5,6 +5,5 @@ filename = 'data/eq_data_1_day_m1.json'
 with open(filename) as f:
     all_eq_data = json.load(f)
 
-readable_file = 'data/readable_eq_data.json'
-with open(readable_file, 'w') as f:
-    json.dump(all_eq_data, f, indent=4)
+all_eq_dicts = all_eq_data['features']
+print(len(all_eq_dicts))
