33afe756be8ce9523981b74539674040d7f252ad
Justin Davis <justn.davis@gmail.com>
2019-10-25 22:02:32 -0400

    Plotting a series of points with scatter()

--- scatter_squares.py
+++ scatter_squares.py
@@ -1,8 +1,11 @@
 import matplotlib.pyplot as plt
 
+x_values = [x for x in range(1,6)]
+y_values = [x**2 for x in x_values]
+
 plt.style.use('seaborn')
 fig, ax = plt.subplots()
-ax.scatter(2, 4, s=200)
+ax.scatter(x_values, y_values, s=100)
 
 # Set chart title and label axes.
 ax.set_title("Square Numbers", fontsize=24)
